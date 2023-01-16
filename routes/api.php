d72de2bd8e3871e14d4c4f7ebfb3b7e97d53ce3c
Rajan <rajan@prepr.org>
2023-01-16 18:41:26 +0530

    remove extra codes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\Auth\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
