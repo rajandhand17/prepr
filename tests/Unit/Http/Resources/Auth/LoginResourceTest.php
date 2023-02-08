d599a5f4e9b0fd8c1f4baaa181a9211f4a843a9e
Rajan <rajan@prepr.org>
2023-02-08 21:47:51 +0530

    login resources

---
+++ tests/Unit/Http/Resources/Auth/LoginResourceTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit\Http\Resources\Auth;
+
+use App\Http\Resources\Auth\LoginResource;
+use Tests\TestCase;
+
+/**
+ * Class LoginResourceTest.
+ *
+ * @covers \App\Http\Resources\Auth\LoginResource
+ */
+final class LoginResourceTest extends TestCase
+{
+    public function testToArray(): void
+    {
+        /** @todo This test is incomplete. */
+        $this->markTestIncomplete();
+    }
+}
