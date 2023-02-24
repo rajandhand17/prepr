76a89fc94d2c0cf2106b8f209cde0ceb858fc51a
Rajan Dhand <rajan@prepr.org>
2023-02-24 20:47:15 +0530

    commit seeders

--- database/seeders/AssignPermissionToRolesSeeder.php
+++ database/seeders/AssignPermissionToRolesSeeder.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Seeders;
 
 use App\Models\Permission;
@@ -41,7 +40,13 @@ class AssignPermissionToRolesSeeder extends Seeder
             //Assign Permissions to organization manager
             $organization_manager = Role::where('name','organization_manager')->first();
             if($organization_manager){
-                $remove_permissions = ['impersonate_user'];
+                $remove_permissions = [
+                    'impersonate_user',
+                    'view_organization',
+                    'create_organization',
+                    'edit_organization',
+                    'delete_organization',
+                ];
                 $final_permissions = $all_permissions;
                 foreach ($remove_permissions as $permission){
                     $final_permissions = $final_permissions->reject(function ($value, $key) use ($permission){
@@ -50,8 +55,7 @@ class AssignPermissionToRolesSeeder extends Seeder
                 }
                 $organization_manager->syncPermissions($final_permissions);
             }
-
-
+            
             //Assign Permissions to lab manager
             $lab_manager = Role::where('name','lab_manager')->first();
             if($lab_manager){
@@ -73,9 +77,135 @@ class AssignPermissionToRolesSeeder extends Seeder
                     });
                 }
                 $lab_manager->syncPermissions($final_permissions);
-            }
-        }
+         
 
+               }
+
+               //Assign Permissions To Challenge Manager
+            $lab_manager = Role::where('name','challenge_manager')->first();
+            if($lab_manager){
+                $remove_permissions = [
+                    'impersonate_user',
+                    'view_organization',
+                    'create_organization',
+                    'edit_organization',
+                    'delete_organization',
+                    'view_organization_members',
+                    'create_organization_members',
+                    'edit_organization_members',
+                    'delete_organization_members',
+                    'view_lab',
+                    'create_lab',
+                    'edit_lab',
+                    'delete_lab',
+                    'generate_lab_report',
+                    'view_lab_member',
+                    'create_lab_member',
+                    'edit_lab_member',
+                    'delete_lab_member',
+                    'view_lab_programs',
+                    'create_lab_programs',
+                    'edit_lab_programs',
+                    'delete_lab_programs',
+                ];
+                $final_permissions = $all_permissions;
+                foreach ($remove_permissions as $permission){
+                    $final_permissions = $final_permissions->reject(function ($value, $key) use ($permission){
+                        return $value->name === $permission;
+                    });
+                }
+                $lab_manager->syncPermissions($final_permissions);
+
+               }
+
+                 //Assign Permissions To Resource Manager
+            $lab_manager = Role::where('name','resource_manager')->first();
+            if($lab_manager){
+                $remove_permissions = [
+                    'impersonate_user',
+                    'view_organization',
+                    'create_organization',
+                    'edit_organization',
+                    'delete_organization',
+                    'view_organization_members',
+                    'create_organization_members',
+                    'edit_organization_members',
+                    'delete_organization_members',
+                    'view_lab',
+                    'create_lab',
+                    'edit_lab',
+                    'delete_lab',
+                    'generate_lab_report',
+                    'view_lab_member',
+                    'create_lab_member',
+                    'edit_lab_member',
+                    'delete_lab_member',
+                    'view_lab_programs',
+                    'create_lab_programs',
+                    'edit_lab_programs',
+                    'delete_lab_programs',
+                    'view_challenge',
+                    'create_challenge',
+                    'edit_challenge',
+                    'delete_challenge',
+                    'generate_challenge_report',
+                    'clone_challenge',
+                    'view_challenge_annoucements',
+                    'create_challenge_annoucements',
+                    'edit_challenge_annoucements',
+                    'delete_challenge_annoucements',
+                    'create_challenge_assessment',
+                    'view_project_submission',
+                    'remove_project_submission',
+                    'view_challenges_member',
+                    'create_challenges_member',
+                    'edit_challenges_member',
+                    'delete_challenges_member',
+                    'view_challenges_path',
+                    'create_challenges_path',
+                    'edit_challenges_path',
+                    'delete_challenges_path',
+                ];
+                $final_permissions = $all_permissions;
+                foreach ($remove_permissions as $permission){
+                    $final_permissions = $final_permissions->reject(function ($value, $key) use ($permission){
+                        return $value->name === $permission;
+                    });
+                }
+                $lab_manager->syncPermissions($final_permissions);
+
+               }
+
+               //Assign Permissions to lab manager
+            $lab_manager = Role::where('name','customer_success')->first();
+            if($lab_manager){
+                $remove_permissions = [
+                    'delete_organization',
+                    'delete_organization_members',
+                    'delete_lab',
+                    'delete_lab_member',
+                    'delete_lab_programs',
+                    'delete_challenge',
+                    'delete_challenge_annoucements',
+                    'delete_project_submission',
+                    'delete_challenges_members',
+                    'delete_challenges_path',
+                    'delete_resource_module',
+                    'delete_resource_collection',
+                    'delete_resource_group',
+                ];
+                $final_permissions = $all_permissions;
+                foreach ($remove_permissions as $permission){
+                    $final_permissions = $final_permissions->reject(function ($value, $key) use ($permission){
+                        return $value->name === $permission;
+                    });
+                }
+                $lab_manager->syncPermissions($final_permissions);
+         
+
+               }               
+               
+    }
 
     }
 }
