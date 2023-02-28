435463b5aacf23ac5a2ccf7300b7824eb26e0f87
Rajan Dhand <rajan@prepr.org>
2023-02-28 16:15:18 +0530

    migrations of organization

--- database/migrations/2023_02_28_093106_add_column_organizations_table.php
+++ database/migrations/2023_02_28_093106_add_column_organizations_table.php
@@ -14,12 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::table('organizations', function (Blueprint $table) {
-            $table->bigIncrements('id');
             $table->integer('user_id')->nullable();
             $table->string('name')->nullable();
             $table->string('slug')->nullable();
             $table->text('vanity_slug')->nullable();
-            $table->text('description')->nullable();
             $table->string('cover_image')->nullable();
             $table->string('profile_image')->nullable();
             $table->string('website')->nullable();
@@ -36,6 +34,7 @@ return new class extends Migration
             $table->integer('challenges_limit')->nullable();
             $table->integer('resources_limit')->nullable();
             $table->tinyInteger('is_auto_created')->nullable();
+            $table->dropColumn('display_name');
             $table->softDeletes();
         });
     }
@@ -48,7 +47,27 @@ return new class extends Migration
     public function down()
     {
         Schema::table('organizations', function (Blueprint $table) {
-            //
+           
+            $table->dropColumn('user_id');
+            $table->dropColumn('name');
+            $table->dropColumn('slug');
+            $table->dropColumn('vanity_slug');
+            $table->dropColumn('cover_image');
+            $table->dropColumn('profile_image');
+            $table->dropColumn('website');
+            $table->dropColumn('about');
+            $table->dropColumn('category');
+            $table->dropColumn('vanity_link');
+            $table->dropColumn('status');
+            $table->dropColumn('magnet_community_id');
+            $table->dropColumn('associat_lab');
+            $table->dropColumn('associat_challenges');
+            $table->dropColumn('plan');
+            $table->dropColumn('plan_validity');
+            $table->dropColumn('labs_limit');
+            $table->dropColumn('challenges_limit');
+            $table->dropColumn('resources_limit');
+            $table->dropColumn('is_auto_created');
         });
     }
 };
