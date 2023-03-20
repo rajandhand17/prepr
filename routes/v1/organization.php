19e5cac605ab0221b1b5a739d267ed1f7e1e0194
Rajan Dhand <rajan@prepr.org>
2023-03-20 21:12:48 +0530

    laratrust

--- routes/v1/organization.php
+++ routes/v1/organization.php
@@ -4,13 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Organization\OrganizationController;
 use Intervention\Image\ImageManagerStatic as Image;
 
-Route::middleware(['language'])->group(function () { 
- 
+Route::middleware(['language'])->group(function (){ 
     Route::get('/',[OrganizationController::class, 'list']);
     Route::get('/view',[OrganizationController::class,'view']);
-    Route::post('/create',[OrganizationController::class, 'create']);
+    //Route::post('/create',[OrganizationController::class, 'create']);
+    Route::post('/create', ['middleware' => ['permission:create_organization'], 'uses' => 'OrganizationController@create']);
+   // Route::post('/create', ['middleware' => ['role:admin'], 'uses' => 'OrganizationController@create']);
     Route::post('/update',[OrganizationController::class, 'update']);
     Route::post('/delete',[OrganizationController::class, 'delete']);
+    
 });
 
 
