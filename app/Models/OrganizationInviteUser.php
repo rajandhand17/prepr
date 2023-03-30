0c95bc230bdf16a70c06c66f17682f2a743c34aa
Rajan Dhand <rajan@prepr.org>
2023-03-30 21:27:42 +0530

    new migrations and organization data migrations

---
+++ app/Models/OrganizationInviteUser.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OrganizationInviteUser extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'organization_invite_users';
+    
+    protected $fillable = [
+        'organisation_id',
+        'user_id',
+        'inviter_id',
+        'image',
+        'lab_ids',
+        'resource_ids',
+        'role',
+        'email',
+        'status',
+        'invite_type',
+        'invitation_status',
+        'invite_status',
+        'email_status',
+        'email_responce',
+        'email_resend_status',
+        'subject_line',
+        'email_message',
+        'fail_schedule',
+    ];
+}
