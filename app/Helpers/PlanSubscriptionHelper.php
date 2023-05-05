1d2948709f2f86a7c1ec87be99618ee8cf43e106
Rajan Dhand <rajan@prepr.org>
2023-05-05 14:51:27 +0530

    changes in helper

--- app/Helpers/PlanSubscriptionHelper.php
+++ app/Helpers/PlanSubscriptionHelper.php
@@ -1,29 +1,18 @@
 <?php
-
 namespace App\Helpers;
 
-use Carbon\Carbon;
-use Session;
-use App\Helpers\Helper;
-use App\Models\User;
-use Illuminate\Support\Facades\Event;
-use Spatie\Permission\Models\Role;
-use Illuminate\Http\Request;
-use Exception;
-use App\Models\Lab;
-use Illuminate\Support\Facades\App;
 use ChargeBee\ChargeBee\Environment;
 use ChargeBee\ChargeBee\Models\Subscription;
 use ChargeBee\ChargeBee\Models\Customer;
-use ChargeBee\ChargeBee\Models\ItemEntitlement;
-
-
 
 class PlanSubscriptionHelper
-{
+{  
+    public function __construct()
+    {
+        Environment::configure(env("Chargebee_Site"),env("Chargebee_Key"));
+    }
     // create customer everytime when new user register 
-    public static function createCustomer($user, $language) {
-        Environment::configure("preprme-test","test_b9uO7I3n4qXg20G7sMfkiyEhwQYdNbd9");
+    public static function createCustomer($user, $language){
         $result = Customer::create(array(
           "firstName" =>  $user->first_name,
           "lastName" => $user->last_name,
@@ -37,7 +26,6 @@ class PlanSubscriptionHelper
 
     //assigning free plan to new user when he register
     public static function freePlanSubscribe($customer) {
-    Environment::configure("preprme-test","test_b9uO7I3n4qXg20G7sMfkiyEhwQYdNbd9");
     $result = Subscription::createWithItems($customer->id,array(
       "subscriptionItems" => array(array(
         "itemPriceId" => "free-plan-CAD-Yearly",
@@ -46,5 +34,8 @@ class PlanSubscriptionHelper
       )
       ));
       $subscription = $result->subscription();
+      if($subscription){
+          return $subscription;
+      }
     }
 }
