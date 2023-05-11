5b62a215c9d1a44a319c8e6a83a2d682151a136f
Rajan Dhand <rajan@prepr.org>
2023-05-11 16:31:49 +0530

    remove authentications from organization route

--- routes/v1/organization.php
+++ routes/v1/organization.php
@@ -4,12 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Organization\OrganizationController;
 use Intervention\Image\ImageManagerStatic as Image;
 
-Route::middleware(['language','auth:api'])->group(function (){ 
-    Route::get('/',[OrganizationController::class,'list'])->middleware('permission:view_organization');
-    Route::get('/{slug}',[OrganizationController::class, 'view'])->middleware('permission:view_organization');
-    Route::post('/create',[OrganizationController::class, 'create'])->middleware('permission:create_organization');
-    Route::put('/{slug}/update',[OrganizationController::class, 'update'])->middleware('permission:edit_organization');
-    Route::delete('/{slug}/delete',[OrganizationController::class, 'delete'])->middleware('permission:delete_organization');
+//Route::middleware(['language','auth:api'])->group(function (){ 
+Route::middleware(['language'])->group(function (){ 
+    Route::get('/',[OrganizationController::class,'list']);//->middleware('permission:view_organization');
+    Route::get('/{slug}',[OrganizationController::class, 'view']);//->middleware('permission:view_organization');
+    Route::post('/create',[OrganizationController::class, 'create']);//->middleware('permission:create_organization');
+    Route::put('/{slug}/update',[OrganizationController::class, 'update']);//->middleware('permission:edit_organization');
+    Route::delete('/{slug}/delete',[OrganizationController::class, 'delete']);//->middleware('permission:delete_organization');
 });
 
 ?>
