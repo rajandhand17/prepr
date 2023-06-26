336db2bbe2794576eef5e451d764149313385c6b
Vinod Kakad <vinod@prepr.org>
2023-06-26 13:46:35 +0530

    Added fixes for component middleware

--- routes/v1/member-management.php
+++ routes/v1/member-management.php
@@ -3,10 +3,10 @@
 use App\Http\Controllers\Api\MemberManagement\MemberManagementController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['language', 'auth:api', 'check.component'])->group(function () {
+Route::middleware(['language', 'auth:api'])->group(function () {
     Route::get('/get-roles ', [MemberManagementController::class, 'getRoles']);
     Route::get('/download-sample ', [MemberManagementController::class, 'downloadSample']);
-    Route::get('/{component}/{slug}', [MemberManagementController::class, 'index']);
-    Route::post('/{component}/{slug}/create ', [MemberManagementController::class, 'create']);
-    Route::post('/{component}/{slug}/delete ', [MemberManagementController::class, 'delete']);
+    Route::get('/{component}/{slug}', [MemberManagementController::class, 'index'])->middleware('check.component');
+    Route::post('/{component}/{slug}/create ', [MemberManagementController::class, 'create'])->middleware('check.component');
+    Route::post('/{component}/{slug}/delete ', [MemberManagementController::class, 'delete'])->middleware('check.component');
 });
