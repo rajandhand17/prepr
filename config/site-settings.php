4f9c3937ed8d4d8c5bd1510db331a863aef8487c
Vinod Kakad <vinod@prepr.org>
2023-07-19 13:40:00 +0530

    Added default images changes

--- config/site-settings.php
+++ config/site-settings.php
@@ -5,33 +5,33 @@ return [
     'listing_limit'                                 => '20',
     'aws_url'                                       => env('AWS_URL'),
 
-    'default_user_profile_image'                    => 'front/img/default-user.png',
+    'default_user_profile_image'                    => 'default_images/user.webp',
 
-    'default_organization_profile_image'            => 'front/img/default-user.png',
-    'default_organization_cover_image'              => 'front/img/default-user.png',
+    'default_organization_profile_image'            => 'default_images/organization.webp',
+    'default_organization_cover_image'              => 'default_images/organization.webp',
 
-    'default_lab_profile_image'                     => 'front/img/default-user.png',
-    'default_lab_cover_image'                       => 'front/img/default-user.png',
+    'default_lab_profile_image'                     => 'default_images/lab.webp',
+    'default_lab_cover_image'                       => 'default_images/lab.webp',
 
-    'default_lab_program_profile_image'             => 'front/img/default-user.png',
-    'default_lab_program_cover_image'               => 'front/img/default-user.png',
+    'default_lab_program_profile_image'             => 'default_images/lab-program.webp',
+    'default_lab_program_cover_image'               => 'default_images/lab-program.webp',
 
-    'default_challenge_profile_image'               => 'front/img/default-user.png',
-    'default_challenge_cover_image'                 => 'front/img/default-user.png',
+    'default_challenge_profile_image'               => 'default_images/challenge.webp',
+    'default_challenge_cover_image'                 => 'default_images/challenge.webp',
 
-    'default_challenge_path_profile_image'          => 'front/img/default-user.png',
-    'default_challenge_path_cover_image'            => 'front/img/default-user.png',
+    'default_challenge_path_profile_image'          => 'default_images/challenge-path.webp',
+    'default_challenge_path_cover_image'            => 'default_images/challenge-path.webp',
 
-    'default_project_profile_image'                 => 'front/img/default-user.png',
-    'default_project_cover_image'                   => 'front/img/default-user.png',
+    'default_project_profile_image'                 => 'default_images/project.webp',
+    'default_project_cover_image'                   => 'default_images/project.webp',
 
-    'default_resource_module_profile_image'         => 'front/img/default-user.png',
-    'default_resource_module_cover_image'           => 'front/img/default-user.png',
+    'default_resource_module_profile_image'         => 'default_images/resource-module.webp',
+    'default_resource_module_cover_image'           => 'default_images/resource-module.webp',
 
-    'default_resource_collection_profile_image'     => 'front/img/default-user.png',
-    'default_resource_collection_cover_image'       => 'front/img/default-user.png',
+    'default_resource_collection_profile_image'     => 'default_images/resource-collection.webp',
+    'default_resource_collection_cover_image'       => 'default_images/resource-collection.webp',
 
-    'default_resource_group_profile_image'          => 'front/img/default-user.png',
-    'default_resource_group_cover_image'            => 'front/img/default-user.png',
+    'default_resource_group_profile_image'          => 'default_images/resource-group.webp',
+    'default_resource_group_cover_image'            => 'default_images/resource-group.webp',
 
 ];
