0e6c2e9942732c6f84ebd6ba4ca3dbb663b6fe42
Rajan Dhand <rajan@prepr.org>
2023-08-18 14:03:38 +0530

    added filter for lab and organization

--- app/Services/Public/LabService.php
+++ app/Services/Public/LabService.php
@@ -32,7 +32,33 @@ class LabService
             if ($request->has('organization_id') && !empty($request->organization_id)) {
                 $lab_list = $lab_list->where('organization_id', '=', $request->organization_id);
             }
-
+            if ($request->has('social_type') && !empty($request->social_type) && $request->social_type == 'liked') {
+                $getLabLikedList = LabSocialActivitiesService::getLabsBasedOnActivity('like');
+                if ($getLabLikedList && $getLabLikedList->count() > 0) {
+                    $lab_list = $lab_list->whereIn('id', $getLabLikedList->pluck('lab_id'));
+                }
+            }
+            if ($request->has('social_type') && !empty($request->social_type) && $request->social_type == 'favourites') {
+                $getLabFavouriteList = LabSocialActivitiesService::getLabsBasedOnActivity('favourite');
+                if ($getLabFavouriteList && $getLabFavouriteList->count() > 0) {
+                    $lab_list = $lab_list->whereIn('id', $getLabFavouriteList->pluck('lab_id'));
+                }
+            }
+            if ($request->has('sort_by') && !empty($request->sort_by)) {
+                switch ($request->sort_by) {
+                    case 'name-a-to-z':
+                        $lab_list = $lab_list->orderBy('labs.title', 'ASC');
+                        break;
+                    case 'name-z-to-a':
+                        $lab_list = $lab_list->orderBy('labs.title', 'DESC');
+                        break;
+                    case 'creation_date':
+                        $lab_list = $lab_list->orderBy('labs.created_at', 'ASC');
+                        break;
+                    default:
+                        $lab_list = $lab_list->orderBy('labs.id', 'ASC');
+                }
+            }
             return $lab_list;
         } catch (\Exception $e) {
             return false;
