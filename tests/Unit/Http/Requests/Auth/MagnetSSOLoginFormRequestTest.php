57a361e0eb838b7bb72f699379c4d8fdce192a27
Vinod Kakad <vinod@prepr.org>
2023-09-14 14:10:10 +0530

    Added code changes

---
+++ tests/Unit/Http/Requests/Auth/MagnetSSOLoginFormRequestTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit\Http\Requests\Auth;
+
+use App\Http\Requests\Auth\MagnetSSOLoginFormRequest;
+use Tests\TestCase;
+
+/**
+ * Class MagnetSSOLoginFormRequestTest.
+ *
+ * @covers \App\Http\Requests\Auth\MagnetSSOLoginFormRequest
+ */
+final class MagnetSSOLoginFormRequestTest extends TestCase
+{
+    public function testAuthorize(): void
+    {
+        /** @todo This test is incomplete. */
+        $this->markTestIncomplete();
+    }
+
+    public function testRules(): void
+    {
+        /** @todo This test is incomplete. */
+        $this->markTestIncomplete();
+    }
+}
