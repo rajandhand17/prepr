a6086b3727a48828875fbb8ddcadc3759ce069b6
Rajan Dhand <rajan@prepr.org>
2023-09-18 11:24:54 +0530

    git pushed for the removing resource route and change the filter function name

--- routes/v1/manage/resource.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\Manage\Resource\ResourceController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(['language', 'auth:api'])->group(function () {
-    Route::post('/create', [ResourceController::class, 'create']);
-});
