e7198787f4548e702d1d88aec9041f05d2866343
Rajan Dhand <rajan@prepr.org>
2023-09-22 14:09:26 +0530

    pushed for the remove extra files from Lab Model

--- app/Models/Lab.php
+++ app/Models/Lab.php
@@ -5,8 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use DB;
-use phpDocumentor\Reflection\Types\Null_;
 
 class Lab extends Model
 {
