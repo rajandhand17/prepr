f368946cc1c7d7eba5aba49b256b7e0d70ea4369
“Hemarao” <hemarao@prepr.org>
2023-09-22 16:26:23 +0530

    Code push for main table entry added on 22nd Sept

--- app/Repositories/Api/Manage/Challenge/ChallengeInterface.php
+++ app/Repositories/Api/Manage/Challenge/ChallengeInterface.php
@@ -5,4 +5,6 @@ namespace App\Repositories\Api\Manage\Challenge;
 interface ChallengeInterface
 {
     public function uploadChallengeCoverImage($image);
+
+    public function createChallenge($request, $upload_cover_image);
 }
