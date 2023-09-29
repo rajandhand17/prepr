de209f545e6d4a6e6ce552b2e8addadeff2bf897
Hemarao Dugana <hemarao@prepr.org>
2023-09-29 17:58:30 +0530

    Code push

--- app/Services/Manage/ChallengeAchievementService.php
+++ app/Services/Manage/ChallengeAchievementService.php
@@ -17,7 +17,7 @@ class ChallengeAchievementService
             }
 
             return $upload_achievement_image;
-        } catch (\Exception $e) {
+        } catch (Exception $th) {
             return false;
         }
     }
@@ -36,7 +36,7 @@ class ChallengeAchievementService
 
             if ($request->winner_achievement_participation !== null) {
                 foreach ($request->winner_achievement_participation as $key => $value) {
-                    $upload_incentive_achievement_image = FileUploadHelper::uploadImageToS3($request->winner_achievement_image[$key], 'achievement');
+                    $upload_incentive_achievement_image = self::uploadChallengeIncentiveAchievementImage($request->winner_achievement_image[$key]);
                     $challengeIncentiveAchievement = new ChallengeAchievement();
                     $challengeIncentiveAchievement->challenge_id = $challenge;
                     $challengeIncentiveAchievement->achievement_type = '1';
@@ -54,6 +54,19 @@ class ChallengeAchievementService
         }
     }
 
+    public static function uploadChallengeIncentiveAchievementImage($image){
+        try {
+            $upload_incentive_image = FileUploadHelper::uploadImageToS3($image, 'achievement');
+            if ($upload_incentive_image == false) {
+                return false;
+            }
+
+            return $upload_incentive_image;
+        } catch (Exception $th) {
+            return false;
+        }
+    }
+
     public static function updateChallengeAchievement($challenge_id, $request, $update_participation_achievement_image)
     {
         try {
@@ -65,12 +78,36 @@ class ChallengeAchievementService
             $challengeAchievement->achievement_image = ($update_participation_achievement_image) ? $update_participation_achievement_image : $challengeAchievement->achievement_image;
             $challengeAchievement->save();
 
-            if (condition) {
-                # code...
+            $challengeIncentiveData = !empty($request->winner_achievement_image) ? array_map(null, $request->winner_achievement_name, $request->winner_achievement_prize, $request->winner_achievement_points, $request->old_winner_achievement_image, $request->winner_achievement_image) : array_map(null, $request->winner_achievement_name, $request->winner_achievement_prize, $request->winner_achievement_points, $request->old_winner_achievement_image);
+            if (!empty($challengeIncentiveData)) {
+                $challengeIncentiveArrayData = array();
+                foreach ($challengeIncentiveData as $key => $value) {
+                    if (!empty($value[0]) && !empty($value[1]) && !empty($value[2]) && !empty($value[3])) {
+                        $upload_incentive_achievement_image = config('site-settings.default_challenge_cover_image');
+                        if (!empty($request->winner_achievement_image)) {
+                            if (array_key_exists($key, $request->winner_achievement_image)) {
+                                $upload_incentive_achievement_image = self::uploadChallengeIncentiveAchievementImage($request->winner_achievement_image[$key]);
+                            } else {
+                                $upload_incentive_achievement_image = $value[3];
+                            }
+                        } else {
+                            $upload_incentive_achievement_image = $value[3];
+                        }
+                        $challengeIncentivesData['challenge_id'] = $challenge_id;
+                        $challengeIncentivesData['achievement_type'] = '1';
+                        $challengeIncentivesData['achievement_name'] = $value[0];
+                        $challengeIncentivesData['achievement_prize'] = $value[1];
+                        $challengeIncentivesData['achievement_points'] = $value[2];
+                        $challengeIncentivesData['achievement_image'] = $upload_incentive_achievement_image;
+                        $challengeIncentiveArrayData[] = $challengeIncentivesData;
+                    }
+                }
+                if (!empty($challengeIncentiveArrayData)) {
+                    ChallengeAchievement::where(['challenge_id' => $challenge_id, 'achievement_type' => '1'])->delete();
+                    ChallengeAchievement::insert($challengeIncentiveArrayData);
+                }
             }
-
-
-
+            return true;
         } catch (Exception $th) {
             return false;
         }
