0ad36db131131d1ad6b9c4bbb39d60d9d815e37f
Hemarao Dugana <hemarao@prepr.org>
2023-10-05 00:36:05 +0530

    Code push

--- app/Repositories/Api/Manage/ChallengePath/ChallengePathInterface.php
+++ app/Repositories/Api/Manage/ChallengePath/ChallengePathInterface.php
@@ -7,4 +7,6 @@ interface ChallengePathInterface
     public function uploadChallengePathMedia($image);
 
     public function uploadAchievementImage($achievementImage);
+
+    public function createChallengePath($upload_cover_image, $upload_achievement_image, $request);
 }
