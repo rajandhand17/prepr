40b2e441df3b98f9a9b67a7839e3e0d9e5090151
Rajan Dhand <rajan@prepr.org>
2023-10-05 16:11:11 +0530

    git pushed for the resource module apis

---
+++ app/Http/Resources/Public/ResourceModule/ResourceModuleResource.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Resources\Public\ResourceModule;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ResourceModuleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        $links = [];
+        $files = [];
+        $document = [];
+        $video = [];
+        $audio = [];
+        if ($this->url) {
+            $links = $this->url;
+        }
+        if ($this->image) {
+            $files = $this->image;
+        }
+        if ($this->document) {
+            $document = $this->document;
+        }
+        if ($this->video) {
+            $video = $this->video;
+        }
+        if ($this->audio) {
+            $audio = $this->audio;
+        }
+
+        return [
+            'id'                                      => $this->uuid,
+            'language'                                => $this->language,
+            'title'                                   => $this->title,
+            'user'                                    => $this->users->first_name.' '.$this->users->last_name,
+            'organization_id'                         => $this->organization_id,
+            'slug'                                    => $this->slug,
+            'description'                             => $this->description,
+            'media_type'                              => $this->media_type,
+            'cover_image'                             => $this->media,
+            'privacy'                                 => ($this->privacy == '1') ? 'yes' : 'no',
+            'status'                                  => ($this->status == '0') ? 'draft' : (($this->status == '1') ? 'published' : 'archive'),
+            'is_global'                               => ($this->is_global == '1') ? 'yes' : 'no',
+            'links'                                   => $links,
+            'files'                                   => $files,
+            'documents'                               => $document,
+            'video'                                   => $video,
+            'audio'                                   => $audio,
+
+        ];
+    }
+}
