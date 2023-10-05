587602fe675482285647d60d6a2bcfa89240350c
Hemarao Dugana <hemarao@prepr.org>
2023-10-06 01:35:13 +0530

    Code push for last pending filter issue fixex

--- app/Services/Public/ChallengeService.php
+++ app/Services/Public/ChallengeService.php
@@ -72,8 +72,8 @@ class ChallengeService
                 }
             }
             if ($request->has('skills') && !empty($request->skills) && is_array($request->skills)) {
-                $challenge_list = $challenge_list->whereIn('labs.id', function ($query) use ($request) {
-                    $query->select('challenge_skills_groups_stacks.lab_id')
+                $challenge_list = $challenge_list->whereIn('challenges.id', function ($query) use ($request) {
+                    $query->select('challenge_skills_groups_stacks.challenge_id')
                     ->from('challenge_skills_groups_stacks')
                     ->whereIn('challenge_skills_groups_stacks.foreign_id', $request->skills)
                         ->where('challenge_skills_groups_stacks.type', '0')
@@ -82,14 +82,37 @@ class ChallengeService
                 })->distinct('challenges.uuid');
             }
             if ($request->has('tags') && !empty($request->tags) && is_array($request->tags)) {
-                $challenge_list = $challenge_list->whereIn('labs.id', function ($query) use ($request) {
-                    $query->select('challenge_tags_groups.lab_id')
+                $challenge_list = $challenge_list->whereIn('challenges.id', function ($query) use ($request) {
+                    $query->select('challenge_tags_groups.challenge_id')
                     ->from('challenge_tags_groups')
                     ->whereIn('challenge_tags_groups.foreign_id', $request->tags)
                         ->where('challenge_tags_groups.type', '0')
                         ->whereNull('challenge_tags_groups.deleted_at')
                         ->distinct();
                 })->distinct('challenges.uuid');
+            }
+
+            if ($request->has('request_status') && !empty($request->request_status)) {
+                if (auth('api')->check()) {
+                    $status_array = ['accepted', 'pending', 'declined'];
+                    if (in_array($request->request_status, $status_array)) {
+                        $challenge_list = $challenge_list->join('member_management', 'challenges.id', '=', 'member_management.module_id')
+                        ->where(['member_management.module_type' => '2', 'member_management.email' => auth('api')->user()->email]);
+                        switch ($request->request_status) {
+                            case 'accepted':
+                                $challenge_list->where('member_management.invite_status', '1');
+                                break;
+                            case 'pending':
+                                $challenge_list->where('member_management.invite_status', '2');
+                                break;
+                            case 'declined':
+                                $challenge_list->where('member_management.invite_status', '3');
+                                break;
+                            default:
+                                $challenge_list;
+                        }
+                    }
+                }
             }
 
             return $challenge_list;
