1d027c09a552ea4b3225f51705f4028ce42d563c
Rajan Dhand <rajan@prepr.org>
2023-10-05 13:56:24 +0530

    added functions in interface

--- app/Repositories/Api/Public/ResourceModule/ResourceModuleInterface.php
+++ app/Repositories/Api/Public/ResourceModule/ResourceModuleInterface.php
@@ -4,5 +4,12 @@ namespace App\Repositories\Api\Public\ResourceModule;
 
 interface ResourceModuleInterface
 {
+    public function getResourceModuleList($request);
+    public function getResourceModuleBasedOnSlug($slug);
 
+    public function checkslug($slug);
+
+    public function checkReview($resource_module_id,$request);
+
+    public function addReview($resource_module_id,$request);
 }
