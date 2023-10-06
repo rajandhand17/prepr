75bb344e378a0e849ae46200702e1658ed79e779
Hemarao Dugana <hemarao@prepr.org>
2023-10-06 09:18:55 +0530

    Code push for manage challenge path detail api implemented

--- app/Models/ChallengePath.php
+++ app/Models/ChallengePath.php
@@ -36,4 +36,82 @@ class ChallengePath extends Model
     {
         return config('site-settings.aws_url').$value;
     }
+
+    public function component_association()
+    {
+        return $this->hasMany(ComponentAssociation::class, 'challenge_path_id', 'id');
+    }
+
+    public function getOrganization()
+    {
+        return $this->belongsTo(Organization::class, 'organization_id', 'id');
+    }
+
+    public function getCategory()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function achievement()
+    {
+        return $this->hasOne(ChallengePathAchievement::class, 'challenge_path_id', 'id');
+    }
+
+    public function tags()
+    {
+        return $this->hasMany(ChallengePathTagGroup::class, 'challenge_path_id', 'id')->where('type', '0');
+    }
+
+    public function tag_groups()
+    {
+        return $this->hasMany(ChallengePathTagGroup::class, 'challenge_path_id', 'id')->where('type', '1');
+    }
+
+    public function skills()
+    {
+        return $this->hasMany(ChallengePathSkillGroupStack::class, 'challenge_path_id', 'id')->where('type', '0');
+    }
+
+    public function skill_groups()
+    {
+        return $this->hasMany(ChallengePathSkillGroupStack::class, 'challenge_path_id', 'id')->where('type', '1');
+    }
+
+    public function skill_stacks()
+    {
+        return $this->hasMany(ChallengePathSkillGroupStack::class, 'challenge_path_id', 'id')->where('type', '2');
+    }
+
+    public function durations()
+    {
+        return $this->belongsTo(Duration::class, 'duration_id', 'id');
+    }
+
+    public function levels()
+    {
+        return $this->belongsTo(Levels::class, 'level_id', 'id');
+    }
+
+    public function favourite()
+    {
+        if (auth('api')->check()) {
+            return ($this->hasMany(ChallengePathSocialActivity::class, 'challenge_path_id', 'id')->where(['favourite' => '1', 'user_id' => auth('api')->user()->id])->count() > 0) ? 'Yes' : 'No';
+        }
+
+        return 'NA';
+    }
+
+    public function liked()
+    {
+        if (auth('api')->check()) {
+            return ($this->hasMany(ChallengePathSocialActivity::class, 'challenge_path_id', 'id')->where(['like_dislike' => '1', 'user_id' => auth('api')->user()->id])->count() > 0) ? 'Yes' : 'No';
+        }
+
+        return 'NA';
+    }
+
+    public function shares()
+    {
+        return $this->hasMany(ChallengePathSocialActivity::class, 'challenge_path_id', 'id')->where('share', '1');
+    }
 }
--- routes/v1/manage/challenge-path.php
+++ routes/v1/manage/challenge-path.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\Manage\ChallengePath\ChallengePathController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['language', 'auth:api'])->group(function () {
+    Route::get('/{slug}', [ChallengePathController::class, 'show']);
     Route::post('/create', [ChallengePathController::class, 'create']);
     Route::get('/check-slug/{slug}', [ChallengePathController::class, 'checkSlug']);
     Route::get('/check-title/{slug}', [ChallengePathController::class, 'checkName']);
