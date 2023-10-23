06833d47898f991b3b0c01af9cf89ab6a59c5b7f
Rajan Dhand <rajan@prepr.org>
2023-10-23 19:20:39 +0530

    added check slug

--- routes/v1/manage/resource-collection.php
+++ routes/v1/manage/resource-collection.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['language', 'auth:api'])->group(function () {
     Route::post('/create', [ResourceCollectionController::class, 'create']);
+    Route::get('/check-slug/{slug}', [ResourceCollectionController::class, 'checkSlug']);
 });
