90ab8594829086239e17616a5b5418d190421a09
Rajan Dhand <rajan@prepr.org>
2023-11-20 15:24:40 +0530

    update route paramters

--- app/Http/Controllers/Api/Public/Achievement/AchievementController.php
+++ app/Http/Controllers/Api/Public/Achievement/AchievementController.php
@@ -53,16 +53,15 @@ class AchievementController extends AppBaseController
         }
     }
 
-    public function downloadCertificate($certificateNumber, $type)
+    public function downloadCertificate($certificate_number, $type)
     {
         try {
-            $downloadFile = $this->achievementRepository->downloadCertificate($certificateNumber, $type);
+            $downloadFile = $this->achievementRepository->downloadCertificate($certificate_number, $type);
             if ($downloadFile) {
                 return $this->sendResponse(null, __('responses.download_certificate'));
             }
-
             return $this->sendError(__('responses.failed_download_certificate'), 500);
-        } catch(\Exception $e) {
+        }catch(\Exception $e) {
             return $this->sendError(__('responses.send_error'), 500);
         }
     }
--- routes/v1/public/achievement.php
+++ routes/v1/public/achievement.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['language'])->group(function () {
     Route::get('/', [AchievementController::class, 'index']);
     Route::get('/{certificateNumber}', [AchievementController::class, 'show']);
-    Route::get('/download-certificate/{certificateNumber}/{type}', [AchievementController::class, 'downloadCertificate']);
+    Route::get('/download-certificate/{certificate_number}/{type}', [AchievementController::class, 'downloadCertificate']);
 });
