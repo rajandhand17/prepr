05622106bee27c420d589d00ec3f53c388b61ec2
Rajan Dhand <rajan@prepr.org>
2023-11-22 22:56:24 +0530

    update catch

--- app/Http/Controllers/Api/Manage/Profile/ProfileController.php
+++ app/Http/Controllers/Api/Manage/Profile/ProfileController.php
@@ -26,7 +26,7 @@ class ProfileController extends AppBaseController
             }
             return $this->sendError(__('responses.not_found_user_profile_detail'), 404);
         } catch(\Exception $e) {
-            return $this->sendError(__('responses.not_found_user_profile_detail'), 404);
+            return $this->sendError(__('responses.send_error'), 500);
         }
     }
 
@@ -38,7 +38,7 @@ class ProfileController extends AppBaseController
             }
             return $this->sendError(__('responses.user_personal_failed'), 404);
         }catch (\Exception $e){
-            return $this->sendError(__('responses.user_personal_failed'), 404);
+            return $this->sendError(__('responses.send_error'), 500);
         }
     }
 }
