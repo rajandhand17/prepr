1afdf7899a5514df7b09ebc8833a6ba2b32da29f
Rajan Dhand <rajan@prepr.org>
2023-12-07 13:07:02 +0530

    before switch branch

--- app/Http/Controllers/Api/Manage/Profile/ProfileController.php
+++ app/Http/Controllers/Api/Manage/Profile/ProfileController.php
@@ -54,7 +54,6 @@ class ProfileController extends AppBaseController
             if($getExperience){
                 return $this->sendResponse(AddExperienceResource::make($getExperience), __('response.user_experience_created'));
             }
-
             return $this->sendError(__('responses.user_experience_failed'), 404);
         } catch(\Exception $e) {
             return $this->sendError(__('responses.send_error'), 500);
