72c044052a4d19aa94f7177bda4c3528fa9f4a69
Rajan Dhand <rajan@prepr.org>
2023-12-21 12:05:04 +0530

    added external link service

--- app/Services/Manage/ChallengeTemplateExternalLinkService.php
+++ app/Services/Manage/ChallengeTemplateExternalLinkService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Manage;
 
 use App\Models\ChallengeExternalLink;
+use App\Models\ChallengeTemplateExternalLink;
 use App\Models\TemplateChallengeExternalLink;
 use Exception;
 
@@ -14,14 +15,13 @@ class ChallengeTemplateExternalLinkService
             $challengeExternalLinks = ChallengeExternalLink::where('challenge_id', $challengeId)->get();
             if ($challengeExternalLinks) {
                 foreach ($challengeExternalLinks as $challengeExternalLink) {
-                    $templateChallengeExternalLink = new TemplateChallengeExternalLink();
-                    $
-                    $templateChallengeExternalLink->template_challenge_id = $templateChallengeId;
-                    $templateChallengeExternalLink->custom_timelines_title = $challengeExternalLink->custom_timelines_title;
-                    $templateChallengeExternalLink->custom_timelines_description = $challengeExternalLink->custom_timelines_description;
-                    $templateChallengeExternalLink->custom_timelines_duration = $challengeExternalLink->custom_timelines_duration;
-                    $templateChallengeExternalLink->schedule_custom_notify = $challengeExternalLink->schedule_custom_notify;
-                    $templateChallengeExternalLink->save();
+                    $challengeTemplateExternalLink = new ChallengeTemplateExternalLink();
+                    $challengeTemplateExternalLink->template_challenge_id = $templateChallengeId;
+                    $challengeTemplateExternalLink->custom_timelines_title = $challengeExternalLink->custom_timelines_title;
+                    $challengeTemplateExternalLink->custom_timelines_description = $challengeExternalLink->custom_timelines_description;
+                    $challengeTemplateExternalLink->custom_timelines_duration = $challengeExternalLink->custom_timelines_duration;
+                    $challengeTemplateExternalLink->schedule_custom_notify = $challengeExternalLink->schedule_custom_notify;
+                    $challengeTemplateExternalLink->save();
                 }
             }
             return true;
