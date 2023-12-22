542ec79b7fc00549c01ad8c3e7884d14f7706cdd
Rajan Dhand <rajan@prepr.org>
2023-12-22 18:55:21 +0530

    resolve issue

--- app/Services/Manage/ComponentAssociationService.php
+++ app/Services/Manage/ComponentAssociationService.php
@@ -698,6 +698,7 @@ class ComponentAssociationService
             }
             if ($request->has('resource_collection_ids') && count($request->resource_collection_ids) > 0) {
                 $getResourceCollection = ResourceCollectionService::getResourceCollectionBasedOnUUIDArray($request->resource_collection_ids);
+
                 $request->merge(['resource_collection_ids' => $getResourceCollection]);
                 if (count($request->resource_collection_ids) > 0) {
                     $existComponentAssociation = ComponentAssociation::where([
@@ -705,8 +706,8 @@ class ComponentAssociationService
                         ['resource_collection_id', '!=', null],
                     ])->pluck('resource_collection_id')->all();
                     $nonExistingIds = array_diff($existComponentAssociation, $request->resource_collection_ids);
-                    $deleteNonExistingComponentAssociation = ComponentAssociation::where('resource_group_id', $resourceGroupId)->whereIn('resource_collection_id', $nonExistingIds)->delete();
-                    $newComponentAssociation = array_diff($request->resource_ids, $existComponentAssociation);
+                     $deleteNonExistingComponentAssociation = ComponentAssociation::where('resource_group_id', $resourceGroupId)->whereIn('resource_collection_id', $nonExistingIds)->delete();
+                    $newComponentAssociation = array_diff($request->resource_collection_ids, $existComponentAssociation);
                     $sequence = ComponentAssociation::where([
                         ['resource_group_id', '=', $resourceGroupId],
                         ['resource_collection_id', '!=', null],
