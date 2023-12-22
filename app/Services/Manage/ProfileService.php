6ae73d09d473e52f69b47b517ab4bd90d217b3ea
Rajan Dhand <rajan@prepr.org>
2023-12-22 13:08:36 +0530

    delete extra files

--- app/Services/Manage/ProfileService.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Services\Manage;
-
-class ProfileService
-{
-}
--- routes/v1/manage/profile.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\Manage\Profile\ProfileController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(['language', 'auth:api'])->group(function () {
-    Route::get('/countries', [ProfileController::class, 'getCountries']);
-    Route::get('/Institutions', [ProfileController::class, 'getInstitutions']);
-});
