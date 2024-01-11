a798083f3b6ec600027e00e4440c32ed7581ba81
Rajan Dhand <rajan@prepr.org>
2024-01-11 13:41:57 +0530

    added changes in friends resource

--- app/Http/Resources/Profile/FriendsResource.php
+++ app/Http/Resources/Profile/FriendsResource.php
@@ -14,34 +14,19 @@ class FriendsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        if($this->reference_id==auth()->user()->id){
-            $data=[
-                'id'           => $this->getFriendsProfileBasedOnUserId->id,
-                'first_name'   => $this->getFriendsProfileBasedOnUserId->first_name,
-                'last_name'    => $this->getFriendsProfileBasedOnUserId->last_name,
-                'full_name'    => $this->getFriendsProfileBasedOnUserId->full_name,
-                'username'     => $this->getFriendsProfileBasedOnUserId->username,
-                'email'        => $this->getFriendsProfileBasedOnUserId->email,
-                'role'         => 'user',
-                'country_code' => $this->getFriendsProfileBasedOnUserId->country_code,
-                'phone_number' => $this->getFriendsProfileBasedOnUserId->phone_number,
-                'profile_image'=> $this->getFriendsProfileBasedOnUserId->profile_image,
-            ];
-        }else{
-            $data=[
-                'id'           => $this->getFriendsProfilebasedOnReference->id,
-                'first_name'   => $this->getFriendsProfilebasedOnReference->first_name,
-                'last_name'    => $this->getFriendsProfilebasedOnReference->last_name,
-                'full_name'    => $this->getFriendsProfilebasedOnReference->full_name,
-                'username'     => $this->getFriendsProfilebasedOnReference->username,
-                'email'        => $this->getFriendsProfilebasedOnReference->email,
-                'role'         => 'user',
-                'country_code' => $this->getFriendsProfilebasedOnReference->country_code,
-                'phone_number' => $this->getFriendsProfilebasedOnReference->phone_number,
-                'profile_image'=> $this->getFriendsProfilebasedOnReference->profile_image,
-            ];
-
-        }
+        $profile = $this->reference_id == auth()->user()->id ? $this->getFriendsProfileBasedOnUserId : $this->getFriendsProfilebasedOnReference;
+        $data = [
+            'id'            => $profile->id,
+            'first_name'    => $profile->first_name,
+            'last_name'     => $profile->last_name,
+            'full_name'     => $profile->full_name,
+            'username'      => $profile->username,
+            'email'         => $profile->email,
+            'role'          => 'user',
+            'country_code'  => $profile->country_code,
+            'phone_number'  => $profile->phone_number,
+            'profile_image' => $profile->profile_image,
+        ];
         return $data;
     }
 }
