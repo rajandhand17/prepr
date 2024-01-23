ebae167db32baafa1b66f7200d1ddaad332abd46
Hemarao Dugana <hemarao@prepr.org>
2024-01-23 16:42:36 +0530

    Code push post migration issue fix

--- database/migrations/2023_06_28_062105_create_labs_table.php
+++ database/migrations/2023_06_28_062105_create_labs_table.php
@@ -38,9 +38,6 @@ return new class() extends Migration {
             $table->enum('is_verified', ['0', '1'])->default('0')->comment('0-> not-verified ,1-> verified');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('organization_id')->references('id')->on('organizations')->onDelete('cascade');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
-            $table->foreign('duration_id')->references('id')->on('durations')->onDelete('cascade');
-            $table->foreign('level_id')->references('id')->on('levels')->onDelete('cascade');
             $table->timestamps();
             $table->softDeletes();
         });
