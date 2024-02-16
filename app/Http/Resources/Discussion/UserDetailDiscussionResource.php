40cf012a68b5c0bb42a482a012d870bc89b90abb
Rajan Dhand <rajan@prepr.org>
2024-02-16 12:33:47 +0530

    added discussion resource

--- app/Http/Resources/Discussion/UserDetailDiscussionResource.php
+++ app/Http/Resources/Discussion/UserDetailDiscussionResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Discussion;
 
+use App\Helpers\UtilityHelper;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,15 @@ class UserDetailDiscussionResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id'                         => $this->id,
+            'preferred_language'         => $this->preferred_language,
+            'first_name'                 => $this->first_name,
+            'last_name'                  => $this->last_name,
+            'full_name'                  => $this->full_name,
+            'username'                   => $this->username,
+            'email'                      => $this->email,
+            'profile_image'              => $this->profile_image,
+        ];
     }
 }
