f94c74f20f17e091578d3e4a61e9cf272cd43f1a
Rajan Dhand <rajan@prepr.org>
2024-02-20 19:44:21 +0530

    added discussion

--- routes/v1/discussion.php
+++ routes/v1/discussion.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Api\Discussion\DiscussionController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['language', 'auth:api'])->group(function () {
-
     Route::get("/{component}/{slug}/",[DiscussionController::class,'index']);
     Route::post("/{component}/{slug}/{action}",[DiscussionController::class,'componentBasedOnAction']);
     Route::delete("/{component}/{slug}/comment/{id}/delete",[DiscussionController::class,'deleteComment']);
