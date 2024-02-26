cd3aea679c52c6e95a7119f546fdaaac69f24369
ashish336b <ashish336b@gmail.com>
2024-02-26 14:37:31 +0545

    translation type fixed

--- app/Http/Resources/Chat/ConversationResource.php
+++ app/Http/Resources/Chat/ConversationResource.php
@@ -25,7 +25,6 @@ class ConversationResource extends JsonResource
             "is_conversation_seen" => $this->is_conversation_seen,
             "default_conversation_name" => $this->default_conversation_name,
             "last_seen_message" => MessageResource::make(data_get($this->lastSeenMessage, 'chat')),
-            "group_photo" => $this->group_photo,
             "is_private" => $this->is_private ? 'yes' : 'no',
         ];
     }
