99c4ef475c6e9831aefd55f27448cf657ecef928
Hemarao Dugana <hemarao@prepr.org>
2024-02-28 23:52:10 +0530

    Code push

--- app/Http/Controllers/Api/Project/ProjectController.php
+++ app/Http/Controllers/Api/Project/ProjectController.php
@@ -86,8 +86,6 @@ class ProjectController extends AppBaseController
 
             return $this->sendError(__('responses.project_list_type'), 400);
         } catch (Exception $e) {
-            dd($e);
-
             return $this->sendError(__('responses.send_error'), 500);
         }
     }
