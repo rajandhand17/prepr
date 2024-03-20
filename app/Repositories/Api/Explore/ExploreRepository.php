d32fc3a2b9606c21a47dacc892122fda4b2272b8
Rajan Dhand <rajan@prepr.org>
2024-03-20 17:06:37 +0530

    added explore controller test

--- app/Repositories/Api/Explore/ExploreRepository.php
+++ app/Repositories/Api/Explore/ExploreRepository.php
@@ -79,7 +79,6 @@ class ExploreRepository implements ExploreInterface
         try {
             $response['labs'] = $this->labService->getTrendingLab();
             $response['challenge'] = $this->challengeService->getTrendingChallenge();
-
             return $response;
         } catch(\Exception $e) {
             return false;
