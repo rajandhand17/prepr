9bd2a2bd18fb379028ead8b1c7350f377966ba86
Hemarao Dugana <hemarao@prepr.org>
2024-04-24 18:27:45 +0530

    Code push

--- app/Helpers/ChargebeeHelper.php
+++ app/Helpers/ChargebeeHelper.php
@@ -2,6 +2,7 @@
 
 namespace App\Helpers;
 
+use App\Services\Manage\ChargebeeSubscriptionService;
 use ChargeBee\ChargeBee\Environment;
 use ChargeBee\ChargeBee\Models\Customer;
 use ChargeBee\ChargeBee\Models\Subscription;
@@ -38,14 +39,14 @@ class ChargebeeHelper
             $subscribePlan = Subscription::createWithItems($user->id, [
                 'subscriptionItems' => [[
                     'itemPriceId' => $planDetail,
-                    'unitPrice'   => 0,
-                    'quantity'    => 1,
                 ],
                 ],
                 'cf_org_id'       => $organization->id,
-                'cf_organisation' => $organization->name,
+                'cf_organisation' => $organization->title,
             ]);
             $subscription = $subscribePlan->subscription();
+
+            self::createChargebeePlanDetails($organization->id);
         } catch(Exception $e) {
             return false;
         }
@@ -131,10 +132,53 @@ class ChargebeeHelper
                     if ($subscriptionEntitlement->featureId == 'user-invite') {
                         $Limits['userInvite'] = $subscriptionEntitlement->value;
                     }
+                    if ($subscriptionEntitlement->featureId == 'pre-built-labs') {
+                        $Limits['preBuiltLab'] = $subscriptionEntitlement->value;
+                    }
                 }
             }
 
             return $Limits;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    // get plan details and their limits from the API.
+    public static function getPlanDetailsAndLimits($organizationId)
+    {
+        try {
+            Environment::configure(config('chargebee.chargebee_site'), config('chargebee.chargebee_key'));
+            $fetchAllSubscriptions = Subscription::all([
+                'cf_org_id[is]' => $organizationId,
+            ]);
+            if ($fetchAllSubscriptions->count() > 0) {
+                $subscription = $fetchAllSubscriptions[0]->subscription();
+                $subscriptionFeatureLimits = self::getFeatureLimits($organizationId);
+                $data = ['subscriptionDetail' => $subscription, 'featureLimits' => $subscriptionFeatureLimits];
+
+                return $data;
+            } else {
+                return $data = [];
+            }
+            
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    public static function createChargebeePlanDetails($organizationId)
+    {
+        try {
+            $fetchChargebeeDetails = self::getPlanDetailsAndLimits($organizationId);
+            if ($fetchChargebeeDetails != []) {
+                $feedChargebeeDetails = ChargebeeSubscriptionService::feedChargebeeDetails($organizationId, $fetchChargebeeDetails);
+                if (!$feedChargebeeDetails) {
+                    return false;
+                }
+            }
+
+            return true;
         } catch (Exception $e) {
             return false;
         }
