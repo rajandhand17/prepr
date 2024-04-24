c5cfbb5afcf57083704d62786743488ff9f78830
Rajan Dhand <rajan@prepr.org>
2024-04-24 16:17:24 +0530

    added lab id

--- app/Services/Manage/ComponentAssociationService.php
+++ app/Services/Manage/ComponentAssociationService.php
@@ -385,7 +385,7 @@ class ComponentAssociationService
                         ['lab_id', '!=', null],
                     ])->select('sequence')->orderBy('id', 'desc')->first();
                     if ($sequences !== null) {
-                        $sequence = $sequence->sequence;
+                        $sequence = $sequences->sequence;
                     }
                     foreach ($newComponentAssociation as $lab_id) {
                         $sequence++;
@@ -400,6 +400,7 @@ class ComponentAssociationService
 
             return true;
         } catch (\Exception $e) {
+            dd($e);
             return false;
         }
     }
