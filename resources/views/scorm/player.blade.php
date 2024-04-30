a19ccb7afc08ff7423a3d1172c0869f986dac741
Suraj Thapa <surajthapa@Surajs-MacBook-Pro.local>
2024-04-30 10:47:02 +0545

    Fav icon on the player

--- resources/views/scorm/player.blade.php
+++ resources/views/scorm/player.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{config('app.name')}}</title>
+    <link rel="icon" href="{{asset('favicon.png')}}">
     @vitereactrefresh
     @vite(['resources/js/scorm-player/app.jsx'])
 </head>
