16d3bbe70df581085d2b0ea42b92ef3b49daef32
Hemarao Dugana <hemarao@prepr.org>
2024-05-02 13:09:26 +0000

    JobTitleService.php edited online with Bitbucket

--- app/Services/JobTitleService.php
+++ app/Services/JobTitleService.php
@@ -59,7 +59,6 @@ class JobTitleService
             return $job_list;
         } catch (Exception $e) {
             Log::error('Error in getJobTitles in JobTitleService.php: '.$e->getMessage());
-            dd($e);
 
             return false;
         }
