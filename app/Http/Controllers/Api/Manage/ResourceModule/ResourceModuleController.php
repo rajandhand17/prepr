6bb604f26797aa6f38a7104c749a7d5ae9b00e56
Rajan Dhand <rajan@prepr.org>
2024-05-03 13:01:15 +0530

    change response status

--- app/Http/Controllers/Api/Manage/ResourceModule/ResourceModuleController.php
+++ app/Http/Controllers/Api/Manage/ResourceModule/ResourceModuleController.php
@@ -159,7 +159,7 @@ class ResourceModuleController extends AppBaseController
                 return $this->sendError(__('responses.resource_module_slug_not_available'));
             }
 
-            return $this->sendResponse([], __('responses.resource_module_slug_available'), 400);
+            return $this->sendResponse([], __('responses.resource_module_slug_available'), 200);
         } catch (\Exception $e) {
             return $this->sendError(__('responses.send_error'), 500);
         }
