e170a2406dcd34bc20986a81094a48326182c8a0
Rajan Dhand <rajan@prepr.org>
2024-05-12 20:22:47 +0530

    added interface functions

--- app/Repositories/Api/Leaderboard/LeaderboardInterface.php
+++ app/Repositories/Api/Leaderboard/LeaderboardInterface.php
@@ -4,5 +4,7 @@ namespace App\Repositories\Api\Leaderboard;
 
 interface LeaderboardInterface
 {
+    public function getLeaderBoardList($request);
 
+    public function getComponentsMembers($slug,$component,$request);
 }
