bb8c9bf80ed63456eccd06cdd27c89623fee0665
Hemarao Dugana <hemarao@prepr.org>
2024-05-23 11:22:05 +0530

    Code push

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -33,7 +33,7 @@ class UserResource extends JsonResource
             $organization_details['id'] = $fetchOrganization->uuid;
             $organization_details['title'] = $fetchOrganization->title;
             $organization_details['slug'] = $fetchOrganization->slug;
-            $organization_details['plan_name'] = __('responses.seed_plan');
+            $organization_details['plan_name'] = 'seed_plan_yearly';
         }
         $memberManagement = new MemberManagementService();
 
