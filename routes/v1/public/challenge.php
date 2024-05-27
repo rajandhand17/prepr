8cf5485d5a99794764d9ee4fe6b4af93e075a596
Rajan Dhand <rajan@prepr.org>
2024-05-27 12:54:07 +0530

    added challengess

--- routes/v1/public/challenge.php
+++ routes/v1/public/challenge.php
@@ -15,5 +15,5 @@ Route::middleware($middleware)->group(function () {
 Route::middleware(['language', 'auth:api'])->group(function () {
     Route::get('/active-challenge/list', [ChallengeController::class, 'challengeList']);
     Route::get('{uuid}/requirements', [ChallengeController::class, 'challengeRequirements']);
-    Route::post('/{slug}/{activity}', [ChallengeController::class, 'socialActivi00000000ty']);
+    Route::post('/{slug}/{activity}', [ChallengeController::class, 'socialActivity']);
 });
