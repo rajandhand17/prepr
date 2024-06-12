8bacff1764922aeb5a6a9c76077d938042222828
Jaspreet k <jaspreet@prepr.org>
2024-06-12 16:35:24 +0530

    LLAI-2676 Skill stack controller, service and route added

--- routes/maestro/skill/skill.php
+++ routes/maestro/skill/skill.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Maestro\Skill\SkillController;
+use App\Http\Controllers\Maestro\Skill\SkillStackController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['web']], function () {
     Route::resource('skills', SkillController::class);
+    Route::resource('skillstack', SkillStackController::class);
 });
