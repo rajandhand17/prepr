a7a26d295c6dc650101bc77669f7b11df9a2a4d0
Rajan Dhand <rajan@prepr.org>
2024-06-12 11:47:37 +0530

    Added organizations

--- app/Http/Controllers/Api/Manage/Organization/OrganizationController.php
+++ app/Http/Controllers/Api/Manage/Organization/OrganizationController.php
@@ -650,11 +650,6 @@ class OrganizationController extends AppBaseController
     public function getOrganizationList(Request $request)
     {
         try {
-            if (!auth()->user()->hasRole([
-                'organization_owner', 'organization_manager', 'lab_manager', 'challenge_manager', 'resource_manager',
-            ])) {
-                return $this->sendError(__('responses.organization_delete_access_denied'), 403);
-            }
             $organization = $this->organizationRepository->getOrganizationListOnlyNameAndUuid($request);
             if ($organization !== false) {
                 return $this->sendResponse($organization, __('responses.found_organization_list'));
