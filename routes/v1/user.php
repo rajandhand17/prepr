db15cc318b8888e80ae05e8f63a4decd51d67c5e
Hemarao Dugana <hemarao@prepr.org>
2024-06-13 13:54:53 +0530

    Code push

--- routes/v1/user.php
+++ routes/v1/user.php
@@ -7,5 +7,6 @@ Route::middleware(['language', 'auth:api'])->group(function () {
     Route::get('/', [UserController::class, 'index']);
     Route::get('/logged-in/details', [UserController::class, 'getLoggedinUser']);
     Route::get('/get-organization-list', [UserController::class, 'getOrganizationList']);
+    Route::get('/get-preferred-organization', [UserController::class, 'getPreferredOrganization']);
     Route::post('/{slug}/organization-preference', [UserController::class, 'setOrganizationPreference']);
 });
