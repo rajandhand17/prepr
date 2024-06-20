85a3b9a74ccdf526d90fac5adb2dd63c0a532aa4
Rajan Dhand <rajan@prepr.org>
2024-06-20 17:33:00 +0530

    Added project member management

--- app/Services/ProjectMemberManagementService.php
+++ app/Services/ProjectMemberManagementService.php
@@ -620,12 +620,4 @@ class ProjectMemberManagementService
         }
     }
 
-    public static function getMembersBasedOnProjectId($requestStatus)
-    {
-        try {
-            // $getProjectIds=ProjectMemberManagement::where(['project_id'=>$requestStatus]);
-        } catch (\Exception $e) {
-            return false;
-        }
-    }
 }
