0fcc6bdfff207c70d5644694f31c4a83d17de059
Hemarao Dugana <hemarao@prepr.org>
2024-06-24 18:00:18 +0530

    Code push for challenge assessment, organization on-boarding issue and resource collection issue fix

--- app/Http/Requests/Manage/Challenge/UpdateChallengeRequest.php
+++ app/Http/Requests/Manage/Challenge/UpdateChallengeRequest.php
@@ -145,16 +145,6 @@ class UpdateChallengeRequest extends FormRequest
             $base_rules['schedule_custom_notify.*'] = 'in:0,1';
         }
 
-        if ($this->has('assessment_title') !== null && $this->has('assessment_score') !== null && $this->has('assessment_weight') !== null) {
-            $base_rules['assessment_title'] = 'array';
-            $base_rules['assessment_title.*'] = 'required';
-            $base_rules['assessment_description'] = 'array';
-            $base_rules['assessment_score'] = 'array';
-            $base_rules['assessment_score.*'] = 'required|numeric';
-            $base_rules['assessment_weight'] = 'array';
-            $base_rules['assessment_weight.*'] = 'required|numeric';
-        }
-
         if ($this->request->has('assessment_type')) {
             $base_rules['assessment_type'] = 'in:open,closed,ai,none';
             $base_rules['guidelines'] = 'required_if:assessment_type,open,closed,ai';
@@ -165,6 +155,26 @@ class UpdateChallengeRequest extends FormRequest
                 $base_rules['members_email'] = 'array|required';
                 $base_rules['members_email.*'] = 'email';
             }
+        }
+
+        if ($this->has('assessment_type') && $this->input('assessment_type') != 'none') {
+            $base_rules['assessment_title'] = 'array';
+            $base_rules['assessment_title.*'] = 'required';
+            $base_rules['assessment_description'] = 'array';
+            $base_rules['assessment_score'] = 'array';
+            $base_rules['assessment_score.*'] = 'required|numeric';
+            $base_rules['assessment_weight'] = 'array';
+            $base_rules['assessment_weight.*'] = 'required|numeric';
+
+            // Custom validation for sum of assessment_weight
+            $base_rules['assessment_weight'] = [
+                'array',
+                function ($attribute, $value, $fail) {
+                    if (array_sum($value) != 100) {
+                        $fail(__('responses.challenge_weight_should_be_100'));
+                    }
+                }
+            ];
         }
 
         if ($this->has('timeline_type') && $this->input('timeline_type') === 'restricted') {
@@ -211,6 +221,23 @@ class UpdateChallengeRequest extends FormRequest
         }
 
         return $base_rules;
+    }
+
+    public function withValidator(Validator $validator)
+    {
+        $validator->after(function ($validator) {
+            $assessment_title = $this->input('assessment_title', []);
+            $assessment_score = $this->input('assessment_score', []);
+
+            $count_title = count($assessment_title);
+            $count_score = count($assessment_score);
+
+            if (($count_title > 0 || $count_score > 0) &&
+                ($count_title !==  $count_score)
+            ) {
+                $validator->errors()->add('assessment_data', __('responses.title_score_should_match_count'));
+            }
+        });
     }
 
     public function failedValidation(Validator $validator)
