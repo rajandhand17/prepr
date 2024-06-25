f7540c97b1de4622af07c3db4b35e48b550376c9
Vinod Kakad <vinodk@precisio.tech>
2024-06-25 13:55:38 +0530

    Added code fix

--- database/migrations/2024_06_20_184433_add_fcm_token_to_users_table.php
+++ database/migrations/2024_06_20_184433_add_fcm_token_to_users_table.php
@@ -11,7 +11,7 @@ return new class() extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('fcm_token')->nullable();
         });
     }
 
@@ -21,7 +21,7 @@ return new class() extends Migration {
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('fcm_token')->nullable();
+            $table->dropColumn('fcm_token')->nullable();
         });
     }
 };
