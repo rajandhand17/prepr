385f01311d932a85f411e7bdf0707b34dd31091c
Rajan Dhand <rajan@prepr.org>
2024-07-01 12:06:05 +0530

    delete extra file

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -68,7 +68,6 @@ class Handler extends ExceptionHandler
 
         if ($e) {
             dd($e);
-
             return Response::json(ResponseUtil::makeError(__('responses.send_error')), 500);
         }
 
