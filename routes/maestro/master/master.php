5e27d1986d871dcbfd43647f42bf9861e7e42726
Rajan Dhand <rajan@prepr.org>
2024-07-08 22:13:33 +0530

    remove commented code

--- routes/maestro/master/master.php
+++ routes/maestro/master/master.php
@@ -7,7 +7,6 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('get-orgs', [MasterController::class, 'getOrganizations'])->name('getOrganizations');
     Route::get('get-categories', [MasterController::class, 'getCategories'])->name('getCategories');
     Route::get('get-skills', [MasterController::class, 'getSkills'])->name('getSkills');
-//    Route::get('get-labs', [MasterController::class, 'getLabs'])->name('getLabs');
     Route::get('get-resource-modules', [MasterController::class, 'getResourceModules'])->name('getResourceModules');
     Route::get('get-users', [MasterController::class, 'getUsers'])->name('getUsers');
     Route::get('get-levels', [MasterController::class, 'getLevels'])->name('getLevels');
