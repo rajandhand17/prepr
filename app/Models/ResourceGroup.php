a68096a489e7c40ac4fb54964ae86131c5d98ae0
Rajan Dhand <rajan@prepr.org>
2024-07-12 15:51:18 +0530

    bug fixes

--- app/Models/ResourceGroup.php
+++ app/Models/ResourceGroup.php
@@ -121,7 +121,14 @@ class ResourceGroup extends Model
 
         return 'NA';
     }
+    public function liked_count()
+    {
+        if (auth('api')->check()) {
+            return $this->hasMany(ResourceGroupSocialActivity::class, 'resource_group_id', 'id')->where(['like_dislike' => '1', 'user_id' => auth('api')->user()->id])->count();
+        }
 
+        return 'NA';
+    }
     public function resource_rating()
     {
         if (auth('api')->check()) {
