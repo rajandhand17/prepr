3c28b8705c64a11909fe5d394d571eaec6e4bb07
vishnu <vishnu@aistechnolabs.org>
2024-07-17 00:06:31 +0530

    User module restructure

--- app/Traits/Maestro/Dashboard/DashboardTrait.php
+++ app/Traits/Maestro/Dashboard/DashboardTrait.php
@@ -2,7 +2,7 @@
 
 namespace App\Traits\Maestro\Dashboard;
 
-use App\Services\Maestro\User\UserService;
+use App\Services\Maestro\UserService;
 use App\Services\Maestro\Project\ProjectService;
 use App\Services\Maestro\Challenge\ChallengeService;
 use App\Services\Maestro\LabService;
