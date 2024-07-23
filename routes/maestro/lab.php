9fa13491a39190d0b8345bbc1902e03271b85a53
Jaspreet k <jaspreet@prepr.org>
2024-07-23 17:03:25 +0530

    lab maestro route

--- routes/maestro/lab.php
+++ routes/maestro/lab.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\Maestro\Lab\LabController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['web']], function () {
+    Route::resource('lab', LabController::class);
     Route::get('labs', [LabController::class, 'getLabsBasedOnOrganization'])->name('getLabsBasedOnOrganization');
 });
