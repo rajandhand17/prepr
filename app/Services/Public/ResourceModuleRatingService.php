a720675529f6ab984be90a6bc9ee859aa1e505be
Rajan Dhand <rajan@prepr.org>
2024-07-24 16:54:32 +0530

    added log in rating function

--- app/Services/Public/ResourceModuleRatingService.php
+++ app/Services/Public/ResourceModuleRatingService.php
@@ -32,6 +32,7 @@ class ResourceModuleRatingService
 
             return $resourceModuleRating;
         } catch (\Exception $e) {
+            UtilityHelper::logError($e);
             return false;
         }
     }
