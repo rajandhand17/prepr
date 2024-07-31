3edbf75a7d19b5e9afe818b719f0a98ad81a767e
vishnu <vishnu@aistechnolabs.org>
2024-07-31 15:38:40 +0530

    Fixed error message issue

--- app/Http/Requests/Manage/LabProgram/CreateLabProgramRequest.php
+++ app/Http/Requests/Manage/LabProgram/CreateLabProgramRequest.php
@@ -32,6 +32,7 @@ class CreateLabProgramRequest extends FormRequest
             'duration_id'             => 'required|exists:durations,id',
             'lab_ids'                 => 'required|array',
             'lab_ids.*'               => 'exists:labs,uuid',
+            'skills.*'                => 'numeric|exists:skills,id',
             'is_sequential'           => 'in:yes,no',
             'privacy'                 => 'in:yes,no',
             'is_achievement_enabled'  => 'in:yes,no',
