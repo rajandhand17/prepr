22dc5b05ba908f87f55301f15bed93c6b81d818d
Hemarao Dugana <hemarao@prepr.org>
2024-08-06 20:21:36 +0530

    Code push for project issue fix

--- app/Services/ChallengeAssessmentUserService.php
+++ app/Services/ChallengeAssessmentUserService.php
@@ -120,19 +120,19 @@ class ChallengeAssessmentUserService
                 $assessment_over_all_comment = $assessmentComment[0];
             }
 
-            switch ($projectData->getProjectAssessment->assessment_type) {
-                case '3':
-                    $assessment = 'ai';
-                    break;
-                case '2':
-                    $assessment = 'closed';
-                    break;
-                case '1':
-                    $assessment = 'open';
-                    break;
-                default:
-                    $assessment = 'none';
-                    break;
+            $assessment = 'none';
+            if ($projectData->getProjectAssessment) {
+                switch ($projectData->getProjectAssessment->assessment_type) {
+                    case '3':
+                        $assessment = 'ai';
+                        break;
+                    case '2':
+                        $assessment = 'closed';
+                        break;
+                    case '1':
+                        $assessment = 'open';
+                        break;
+                }
             }
 
             return [
