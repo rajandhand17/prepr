084f259a91a5c7186fc64c83458b37d528eb6f53
aashish.bhandari <aashish@prepr.org>
2024-08-07 16:37:07 +0545

    LLAI-3141
    - [x] Fix the issue in discussion fetch api.
    - [x] Feat: Added new sort by most replies in discussion

--- app/Services/DiscussionService.php
+++ app/Services/DiscussionService.php
@@ -46,6 +46,11 @@ class DiscussionService
                     ->orderByDesc('dislikes_count');
             }
 
+            if ($sortBy == 'most_replies') {
+                $getComments = $getComments->withCount('comments_reply')->orderBy('comments_reply_count', 'DESC');
+
+            }
+
             return $getComments;
         } catch (\Exception $e) {
             UtilityHelper::logError($e);
