449a66bd35b2c7d0c9a29f6e042806adc253e1d6
Rajan Dhand <rajan@prepr.org>
2024-08-12 19:14:08 +0530

    added auth in route

--- routes/maestro/auto-create-template.php
+++ routes/maestro/auto-create-template.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Maestro\AutoCreateTemplate\AutoCreateTemplateController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['web']], function () {
+Route::group(['middleware' => ['web','auth-check']], function () {
     Route::resource('auto-create', AutoCreateTemplateController::class);
     Route::post('clone-module', [AutoCreateTemplateController::class, 'cloneModule'])->name('cloneInfo');
     Route::post('get-module-list', [AutoCreateTemplateController::class, 'getModuleList'])->name('getModuleList');
