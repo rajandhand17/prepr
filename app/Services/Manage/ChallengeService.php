c8babdf7c22ba51ee45c4943fc4b292bbf34573b
aashish.bhandari <aashish@prepr.org>
2024-08-21 09:51:26 +0545

    Changes applied as per code review.

--- app/Services/Manage/ChallengeService.php
+++ app/Services/Manage/ChallengeService.php
@@ -663,6 +663,7 @@ class ChallengeService
     {
         try {
             $challenge_list = Challenge::select('uuid', 'title', 'media_type', 'media')->where(['organization_id' => $organization->id, 'is_accessible' => '1']);
+            $challenge_list = self::filterChallengeList($challenge_list, $request);
             $limit = config('site-settings.listing_limit');
 
             return $challenge_list->limit($limit)->get();
