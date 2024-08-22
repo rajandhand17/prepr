4aaf41ae49345412d5015b567db16d64d7e0adc3
aashish.bhandari <aashish@prepr.org>
2024-08-22 10:27:04 +0545

    Changes applied after code review

--- app/Http/Controllers/Api/Manage/MemberManagement/MemberManagementController.php
+++ app/Http/Controllers/Api/Manage/MemberManagement/MemberManagementController.php
@@ -88,8 +88,8 @@ class MemberManagementController extends AppBaseController
             if ($component != 'organization' && $request->role != 'User') {
                 return $this->sendError(__('responses.select_valid_role_error'), 422);
             }
-            if ($component === 'challenge' && (int) $checkComponentBasedOnSlug->status === 0) {
-                return $this->sendError(__('responses.request_not_exist'), 404);
+            if ((int) $checkComponentBasedOnSlug->status === 0) {
+                return $this->sendError(__('responses.request_component_draft_stage', ['component' => $component]), 404);
             }
 
             $memberManagementListing = $this->memberManagementRepository->getMembers($checkComponentBasedOnSlug, $component, new Request());
