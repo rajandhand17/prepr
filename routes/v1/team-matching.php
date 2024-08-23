0679371f70fdc0ef3080d619419327a2a319d179
vishnu <vishnu@aistechnolabs.org>
2024-08-23 15:08:17 +0530

    Fixed route requested changes

--- routes/v1/team-matching.php
+++ routes/v1/team-matching.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\Api\TeamMatching\TeamMatchingController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['language', 'auth:api'])->group(function () {
+    Route::post('/profile-check', [TeamMatchingController::class, 'getTeamMatchingProfileCheck']);
+    Route::get('/count', [TeamMatchingController::class, 'getTeamMatchingCount']);
     Route::get('/{action}', [TeamMatchingController::class, 'browseMatchedPendingRequests']);
     Route::get('/{slug}/send-request', [TeamMatchingController::class, 'sendRequest']);
-    Route::post('/team-matching-profile-check', [TeamMatchingController::class, 'getTeamMatchingProfileCheck']);
-    Route::get('count/pending-request-and-matched-item-count', [TeamMatchingController::class, 'getTeamMatchingCount']);
 });
