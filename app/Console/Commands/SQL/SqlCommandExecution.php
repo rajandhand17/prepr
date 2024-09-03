325e0fe09033a87e6a6277103fb78415baf380ba
Hemarao Dugana <hemarao@prepr.org>
2024-09-03 16:23:58 +0530

    Code push

--- app/Console/Commands/SQL/SqlCommandExecution.php
+++ app/Console/Commands/SQL/SqlCommandExecution.php
@@ -31,17 +31,18 @@ class SqlCommandExecution extends Command
     {
         try {
             Schema::disableForeignKeyConstraints();
-            DB::table('resource_modules')->truncate();
-            DB::table('resource_module_details')->truncate();
-            DB::table('resource_module_ratings')->truncate();
-            DB::table('resource_module_skills_groups_stacks')->truncate();
-            DB::table('resource_module_social_activities')->truncate();
-            DB::table('resource_module_tags_groups')->truncate();
-            DB::table('resource_module_type_modes')->truncate();
-            DB::table('resource_module_visits')->truncate();
-            DB::table('scorm')->truncate();
-            DB::table('scorm_sco')->truncate();
-            DB::table('scorm_sco_tracking')->truncate();
+            DB::statement("ALTER TABLE `lab_programs` CHANGE `category_id` `category_id` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `duration_id` `duration_id` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `level_id` `level_id` BIGINT UNSIGNED NULL DEFAULT NULL;");
+            DB::statement("ALTER TABLE `challenge_paths` CHANGE `category_id` `category_id` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `duration_id` `duration_id` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `level_id` `level_id` BIGINT UNSIGNED NULL DEFAULT NULL;");
+            DB::statement("ALTER TABLE `resource_modules` CHANGE `duration_id` `duration_id` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `level_id` `level_id` BIGINT UNSIGNED NULL DEFAULT NULL;");
+            DB::statement("ALTER TABLE `resource_collections` CHANGE `duration` `duration` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `level` `level` BIGINT UNSIGNED NULL DEFAULT NULL;");
+            DB::statement("ALTER TABLE `resource_groups` CHANGE `duration` `duration` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `level` `level` BIGINT UNSIGNED NULL DEFAULT NULL;");
+            DB::statement("ALTER TABLE `challenges` ADD `total_share` INT NULL DEFAULT NULL AFTER `views_count`;");
+            DB::statement("ALTER TABLE `projects` ADD `total_share` INT NULL DEFAULT NULL AFTER `views_count`;");
+            DB::statement("ALTER TABLE `projects` CHANGE `media_type` `media_type` ENUM('0','1','2') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0' COMMENT '0 -> Image, 1-> embedded, Type of media defining of media, 2-> Video', CHANGE `challenge_id` `challenge_id` BIGINT UNSIGNED NULL DEFAULT NULL;");
+            DB::statement("ALTER TABLE `project_pitch_values` CHANGE `pitch_template_id` `pitch_template_id` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `project_pitch_id` `project_pitch_id` BIGINT UNSIGNED NULL DEFAULT NULL;");
+            DB::statement("ALTER TABLE `project_task_values` CHANGE `task_template_id` `task_template_id` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `project_task_id` `project_task_id` BIGINT UNSIGNED NULL DEFAULT NULL;");
+            DB::statement("ALTER TABLE `challenge_templates` CHANGE `category_id` `category_id` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `duration_id` `duration_id` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `level_id` `level_id` BIGINT UNSIGNED NULL DEFAULT NULL;");
+            DB::statement("ALTER TABLE `challenge_path_templates` CHANGE `category_id` `category_id` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `duration_id` `duration_id` BIGINT UNSIGNED NULL DEFAULT NULL, CHANGE `level_id` `level_id` BIGINT UNSIGNED NULL DEFAULT NULL;");
             Schema::enableForeignKeyConstraints();
             $this->info('Sql command executed successfully.');
         } catch (Exception $e) {
