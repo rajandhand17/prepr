3ddf0ba0400ed845476c8a27246956ad879f71be
Rajan Dhand <rajan@prepr.org>
2024-09-04 15:03:24 +0530

    remove if condition

--- app/Helpers/CampusConnectHelper.php
+++ app/Helpers/CampusConnectHelper.php
@@ -139,7 +139,6 @@ class CampusConnectHelper
     {
         try {
             $response = self::post('/curatedContent', $data);
-            if ($response != false) {
                 if ($response->status() === 409) {
                     $updateResponse = self::put('/curatedContent', $data);
                     if ($updateResponse->status() >= 400) {
@@ -152,8 +151,6 @@ class CampusConnectHelper
                 if ($response->status() >= 400) {
                     return false;
                 }
-            }
-
             return true;
         } catch (Exception $exception) {
             UtilityHelper::logError($exception);
