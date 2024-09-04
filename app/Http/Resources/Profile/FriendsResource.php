61546059098920927cd4c3687f4f10b6121f3515
Jaspreet k <jaspreet@prepr.org>
2024-09-04 16:35:25 +0530

    made friends api public

--- app/Http/Resources/Profile/FriendsResource.php
+++ app/Http/Resources/Profile/FriendsResource.php
@@ -14,7 +14,7 @@ class FriendsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $profile = $this->reference_id == auth()->user()->id ? $this->getFriendsProfileBasedOnUserId : $this->getFriendsProfilebasedOnReference;
+        $profile = $this->reference_id == auth()->id() ? $this->getFriendsProfileBasedOnUserId : $this->getFriendsProfilebasedOnReference;
         $data = [];
         if ($profile) {
             $data = [
