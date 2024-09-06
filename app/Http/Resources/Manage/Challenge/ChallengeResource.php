d82c015b9ddc93468aa65db9a2fe68f645775987
aashish.bhandari <aashish@prepr.org>
2024-09-06 10:55:37 +0545

    Conflict resolved.

--- app/Http/Resources/Manage/Challenge/ChallengeResource.php
+++ app/Http/Resources/Manage/Challenge/ChallengeResource.php
@@ -382,16 +382,17 @@ class ChallengeResource extends JsonResource
                     $isActive = 'late_submitted';
                     break;
             }
-        $source = "organization";
-        $source = 'organization';
-
-        if ($this->is_auto_created == '1') {
-            $source = 'onboarding challenge';
-        } elseif ($this->user_id == auth('api')->user()->id && $this->is_pre_built == '1') {
-            $source = 'cloned by you';
-        } elseif ($this->user_id == auth('api')->user()->id) {
-            $source = 'created by you';
         }
+
+            $source = 'Created By Organization';
+
+            if ($this->is_auto_created == '1') {
+                $source = 'Onboarding Challenge';
+            } elseif ($this->user_id == auth('api')->user()->id && $this->is_pre_built == '1') {
+                $source = 'Cloned By You';
+            } elseif ($this->user_id == auth('api')->user()->id) {
+                $source = 'Created By You';
+            }
 
         return [
             'id'                                => $this->uuid,
