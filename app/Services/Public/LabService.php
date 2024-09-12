1e664b3ba424da40c524d287630a4a1d367e60fa
Hemarao Dugana <hemarao@prepr.org>
2024-09-12 23:35:29 +0530

    Code push for LLAI-3667, LLAI-3666 ticket fixtures

--- app/Services/Public/LabService.php
+++ app/Services/Public/LabService.php
@@ -134,7 +134,8 @@ class LabService
                     $status_array = ['accepted', 'pending', 'declined'];
                     if (in_array($request->request_status, $status_array)) {
                         $lab_list = $lab_list->join('member_management', 'labs.id', '=', 'member_management.module_id')
-                            ->where(['member_management.module_type' => '1', 'member_management.email' => auth('api')->user()->email]);
+                            ->where(['member_management.module_type' => '1', 'member_management.email' => auth('api')->user()->email])
+                            ->whereNull('member_management.deleted_at');
                         switch ($request->request_status) {
                             case 'invited':
                                 $lab_list->where('member_management.invite_status', '0');
