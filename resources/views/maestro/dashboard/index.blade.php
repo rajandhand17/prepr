f935c6c1433d1f0f6230eaacab48cce6ced234f6
vishnu <vishnu@aistechnolabs.org>
2024-09-13 11:10:54 +0530

    Fixed redirect issue

--- resources/views/maestro/dashboard/index.blade.php
+++ resources/views/maestro/dashboard/index.blade.php
@@ -80,7 +80,7 @@
                     <div class="icon">
                         <i class="ion ion-pie-graph"></i>
                     </div>
-                    <a href="{{ route('users.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ route('lab.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
