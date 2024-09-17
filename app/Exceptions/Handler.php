58c6448c1d47930fd847496be7fd6394729e9720
Pravin Banjade <pravin@prepr.org>
2024-09-17 15:35:16 +0545

    testing with err handling on

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -56,6 +56,7 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $e)
     {
+        dd($e);
         if ($this->shouldReport($e)) {
             UtilityHelper::logError($e);
         }
