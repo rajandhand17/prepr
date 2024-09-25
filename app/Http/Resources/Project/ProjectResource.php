63dc499d087e60ae136d266306391e7da1b03c40
Hemarao Dugana <hemarao@prepr.org>
2024-09-25 15:29:07 +0530

    Code push for lab details in project reponse

--- app/Http/Resources/Project/ProjectResource.php
+++ app/Http/Resources/Project/ProjectResource.php
@@ -181,7 +181,17 @@ class ProjectResource extends JsonResource
         if ($this->lab_id) {
             $lab_details = LabService::getLabBasedOnId($this->lab_id);
             if ($lab_details) {
-                $lab_details = $lab_details->only(['id', 'uuid', 'title', 'slug', 'description', 'media', 'media_type', 'privacy', 'status']);
+                $lab_details = [
+                    'id'    => $lab_details->id,
+                    'uuid'  => $lab_details->uuid,
+                    'title' => $lab_details->title,
+                    'slug'  => $lab_details->slug,
+                    'description'  => $lab_details->description,
+                    'media'  => $lab_details->media,
+                    'media_type'  => $lab_details->media_type,
+                    'privacy'  => ($lab_details->privacy == '1') ? 'yes' : 'no',
+                    'status'  => ($lab_details->status == '0') ? 'draft' : (($this->status == '1') ? 'published' : 'archive'),
+                ];
             }
         }
 
