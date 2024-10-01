9f5e22177ff0e3ee369472ed9bf43ecff8aabad6
Rajan Dhand <rajan@prepr.org>
2024-10-01 13:44:46 +0530

    added new migration

--- database/migrations/2024_09_27_052552_update_value_column_to_settings_table.php
+++ database/migrations/2024_09_27_052552_update_value_column_to_settings_table.php
@@ -11,7 +11,7 @@ return new class() extends Migration {
     public function up(): void
     {
         Schema::table('settings', function (Blueprint $table) {
-            $table->text('value')->change();
+            $table->longText('value')->change();
         });
     }
 
